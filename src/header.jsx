93ec01e8f12946755b7916d5e36aba9f61ee8c2f
joet <205975513+joetmp4@users.noreply.github.com>
2025-09-27 21:50:58 -0400

    Beginning of Frontend, some of the Main page created

---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header>
+            <h1>Voice To Notes</h1>
+            <h3>Convert any voice recording into flashcards quickly</h3>
+            <p>Powered by OpenAI Whisper and GPT-4</p>
+        </header>
+    );
+}
+
+export default Header;
